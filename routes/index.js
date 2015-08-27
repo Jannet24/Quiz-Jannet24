25ebdd23cd4a83526858871e6040128caec95c9d
Jannet24 <letty_217@hotmail.com>
2015-08-27 00:51:41 -0500

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', 								quizController.index);
+router.get('/quizes/:quizId(\\d+)', 				quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',			quizController.answer);
 module.exports = router;
