437cce9524bd0d37481147596c1f3a7fe593a028
Jannet24 <letty_217@hotmail.com>
2015-08-29 10:44:51 -0500

    Comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz.controller');
+var commentController = require('../controllers/comment.controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors:[] });
@@ -18,5 +20,9 @@ router.post('/quizes/create',              	quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        	quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+
+// Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
 module.exports = router;
